c4a82432d4844fe57cdd0e1855b438da32526192
Ahmadreza Shojaee <ashojaee19@gmail.com>
2025-08-27 13:59:50 +0100

    Update demo_Extract.m

--- demo_Extract.m
+++ demo_Extract.m
@@ -3,5 +3,5 @@ addpath('.../src');
 % Change file path to your SR3 file
 filePath = 'CASE.sr3';
 
-[sr3, Paths] = read_sr3(filePath);
+[sr3, Paths] = read_SR3(filePath);
 [DATA, meta] = extract_spatial_from_sr3(sr3, Paths);
