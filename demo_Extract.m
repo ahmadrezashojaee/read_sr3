6dbc12f18b0460795cf69ca56bd37d35bd8e0f6c
Ahmadreza Shojaee <ashojaee19@gmail.com>
2025-08-27 14:08:52 +0100

    Update demo_Extract.m

--- demo_Extract.m
+++ demo_Extract.m
@@ -1,7 +1,7 @@
 addpath('src');
 
 % Change file path to your SR3 file
-filePath = 'CASE.sr3';
+filePath = '1D_calcite - Flood.sr3';
 
 [sr3, Paths] = read_SR3(filePath);
 [DATA, meta] = extract_spatial_from_sr3(sr3, Paths);
