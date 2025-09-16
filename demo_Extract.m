736f38951b5017f543080e6d56b193902d2ba515
Ahmadreza Shojaee <ashojaee19@gmail.com>
2025-09-16 12:55:14 +0100

    Update demo_Extract.m

--- demo_Extract.m
+++ demo_Extract.m
@@ -6,3 +6,4 @@ filePath = 'YourSR3File.sr3';
 
 [sr3, Paths] = read_SR3(filePath);
 [DATA, meta] = extract_spatial_from_sr3(sr3, Paths, filePath);
+[WELL_DATA, time_days, time_date, meta_wells] = extract_well_from_sr3(sr3, filePath, 1000);
